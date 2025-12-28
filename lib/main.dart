6b3e403b95facf9e7993149247cb6862309210cc
Sherif Ashraf <sherifashraf1054@gmail.com>
2025-12-28 10:44:34 +0200

    Initial Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
