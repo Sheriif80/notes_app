705009dd20add006d7e4b78c4d77efff16797b70
Sherif80 <sherifashraf1054@gmail.com>
2026-01-03 15:12:11 +0200

    finished the color list view

--- lib/views/widgets/color_item.dart
+++ lib/views/widgets/color_item.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isPicked, required this.color});
+  final bool isPicked;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 4),
-      child: CircleAvatar(radius: 25, backgroundColor: Colors.lightBlue),
-    );
+    return isPicked
+        ? Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            child: CircleAvatar(
+              radius: 28,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(radius: 25, backgroundColor: color),
+            ),
+          )
+        : Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 2),
+            child: CircleAvatar(radius: 25, backgroundColor: color),
+          );
   }
 }
--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/color_item.dart';
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
   @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  final List<Color> colors = const [
+    Color(0xff042601),
+    Color(0xffFDFFFC),
+    Color(0xff2EC4B6),
+    Color(0xffE71D36),
+    Color(0xffFF9F1C),
+    Color(0xffD5BF86),
+    Color(0xff3F0D12),
+    Color(0xffE1EFE6),
+  ];
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 25 * 2,
+      height: 28 * 2,
       child: ListView.builder(
         scrollDirection: .horizontal,
-        itemCount: 8,
+        itemCount: colors.length,
         itemBuilder: (context, index) {
-          return const ColorItem();
+          return GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              setState(() {});
+            },
+            child: ColorItem(
+              isPicked: index == currentIndex,
+              color: colors[index],
+            ),
+          );
         },
       ),
     );
